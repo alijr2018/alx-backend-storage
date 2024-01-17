68eb2488d78d991ce1d34d7e397fb3fd2da716ee
alijr2018 <alijr_2018@proton.me>
2024-01-17 20:39:21 +0100

    check

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- 0-uniq_users.sql
+-- Write a SQL script that creates a table users.
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);

2018184244989328d1782a16b585c3eb066334e1
alijr2018 <alijr_2018@proton.me>
2024-01-17 20:49:09 +0100

    fix

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- 2-fans.sql
 -- Write a SQL script that ranks country origins of bands
-SELECT origin, SUM(nb_fans) AS nb_fans
+SELECT origin, COUNT(DISTINCT fan_id) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;

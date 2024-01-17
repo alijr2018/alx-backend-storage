07e950c97109e4aae097b0723305eae64bd8a003
alijr2018 <alijr_2018@proton.me>
2024-01-17 22:11:50 +0100

    fix 9

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,4 @@
 -- 9-index_name_score.sql
 
 -- Write a SQL script that creates an index idx_name_first_score on the table names and the first letter of name and the score.
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
+CREATE INDEX idx_name_first_score ON names(name(1), score);

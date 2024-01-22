f83ce98d9fa54e6815764017e18ba1325fb0d95c
alijr2018 <alijr_2018@proton.me>
2024-01-23 00:08:27 +0100

    fixing 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -10,4 +10,4 @@ def update_topics(mongo_collection, name, topics):
     a Python function that changes all topics of a school,
     document based on the name.
     """
-    return mongo_collection.update({'name': name}, {'$set': {'topics': topics}})
+    return mongo_collection.update({"name": name}, {"$set": {"topics": topics}})

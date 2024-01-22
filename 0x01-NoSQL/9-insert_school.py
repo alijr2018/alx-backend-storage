1baf30f3fc7172a58230c3e7d04a599dcb9ac5c9
alijr2018 <alijr_2018@proton.me>
2024-01-23 00:00:05 +0100

    fixing 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -10,4 +10,4 @@ def insert_school(mongo_collection, **kwargs):
     a Python function that inserts a,
     new document in a collection based on kwargs.
     """
-    return (mongo_collection.inserts(kwargs))
+    return (mongo_collection.insert(kwargs))

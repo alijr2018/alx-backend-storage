6eae932617e4e50a4899af80f91e0ec9b2233bbf
alijr2018 <alijr_2018@proton.me>
2024-01-23 22:55:25 +0100

    102-log_stats.py

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+102-log_stats.py
+"""
+import pymongo
+
+
+def log_stats():
+    """
+    Improve 12-log_stats.py by adding the top 10,
+    of the most present IPs in the collection nginx,
+    of the database logs.
+    """
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["logs"]
+    collection = db["nginx"]
+
+    total_logs = collection.count_documents({})
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    methods_counts = {method: collection.count_documents({"method": method.lower()}) for method in methods}
+
+    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+
+    top = list(collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+        ]))
+
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method in methods:
+        print(f"\tmethod {method}: {methods_counts[method]}")
+    print(f"{status_check_count} status check")
+    print("IPs:")
+    for ip_info in top:
+        print(f"\t{ip_info['_id']}: {ip_info['count']}")
+
+if __name__ == "__main__":
+    log_stats()

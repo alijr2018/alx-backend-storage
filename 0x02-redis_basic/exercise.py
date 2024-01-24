126b86ef1e83681939e61ecb18429f354d63507c
alijr2018 <alijr_2018@proton.me>
2024-01-24 18:27:45 +0100

    2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,13 +5,25 @@ exercise.py
 import redis
 import uuid
 from typing import Callable, Union
-
+from functools import wraps
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @staticmethod
+    def count_calls(method: Callable) -> Callable:
+        key = method.__qualname__
+
+        @wraps(method)
+        def wrapper(self, *args, **kwargs):
+            self._redis.incr(key)
+            return method(self, *args, **kwargs)
+
+        return wrapper
+
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
@@ -24,22 +36,17 @@ class Cache:
         return value
 
     def get_str(self, key: str) -> Union[str, None]:
-        return self.get(
-            key, fn=lambda d: d.decode("utf-8") if d is not None else None)
+        return self.get(key, fn=lambda d: d.decode("utf-8") if d is not None else None)
 
     def get_int(self, key: str) -> Union[int, None]:
         return self.get(key, fn=lambda d: int(d) if d is not None else None)
 
-
 if __name__ == "__main__":
     cache = Cache()
 
-    TEST_CASES = {
-        b"foo": None,
-        123: int,
-        "bar": lambda d: d.decode("utf-8")
-    }
+    cache.store(b"first")
+    print(cache.get(cache.store.__qualname__))
 
-    for value, fn in TEST_CASES.items():
-        key = cache.store(value)
-        assert cache.get(key, fn=fn) == value
+    cache.store(b"second")
+    cache.store(b"third")
+    print(cache.get(cache.store.__qualname__))

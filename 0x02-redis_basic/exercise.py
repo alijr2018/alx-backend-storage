bde1c93afab9cff1d7a885ae9c933c0fd44de6fb
alijr2018 <alijr_2018@proton.me>
2024-01-24 11:12:10 +0100

    1. Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,7 +16,7 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-    
+
     def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, None]:
         value = self._redis.get(key)
         if value is not None and fn is not None:
@@ -24,20 +24,23 @@ class Cache:
         return value
 
     def get_str(self, key: str) -> Union[str, None]:
-        return self.get(key, fn=lambda d: d.decode("utf-8") if d is not None else None)
+        return self.get(
+            key, fn=lambda d: d.decode("utf-8") if d is not None else None
+            )
 
     def get_int(self, key: str) -> Union[int, None]:
         return self.get(key, fn=lambda d: int(d) if d is not None else None)
 
+
 if __name__ == "__main__":
     cache = Cache()
 
-    TEST_CASES = {
+TEST_CASES = {
     b"foo": None,
     123: int,
     "bar": lambda d: d.decode("utf-8")
-    }
+}
 
-    for value, fn in TEST_CASES.items():
+for value, fn in TEST_CASES.items():
     key = cache.store(value)
     assert cache.get(key, fn=fn) == value
